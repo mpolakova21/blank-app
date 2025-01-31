8e5030a7f73712dee1a4f8be56201643f866f248
mpolakova21 <mpolakova21@gmail.com>
2025-01-31 14:11:48 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+st.write(df)
+
+number = st.slider("Vyberte číslo", 0, 100, 3)
+st.write(f"Vybrané číslo: {number}")
